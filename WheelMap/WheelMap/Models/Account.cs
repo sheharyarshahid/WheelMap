a02de684428bfe6a91f1faa959416e747df3de97
Sheharyar Shahid <sshary019@gmail.com>
2020-05-17 03:34:20 +0500

    Craeted Account model that contains user email, name and password

---
+++ WheelMap/WheelMap/Models/Account.cs
@@ -0,0 +1,23 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WheelMap.Models
+{
+    [Table("Accounts")]
+    public class Account
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        [MaxLength(25), NotNull]
+        public string Name { get; set; }
+
+        [MaxLength(25), NotNull]
+        public string Email { get; set; }
+
+        [MaxLength(25), NotNull]
+        public string Password { get; set; }
+    }
+}
