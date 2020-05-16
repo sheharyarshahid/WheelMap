80aa15cdf46a9457dc9e283ae0179c4615d41c19
Sheharyar Shahid <sshary019@gmail.com>
2020-05-17 03:34:00 +0500

    Added file path service that returns android local path to save local database file into that location.

---
+++ WheelMap/WheelMap.Android/Services/FilePathService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using WheelMap.Droid.Services;
+using WheelMap.Services;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(FilePathService))]
+namespace WheelMap.Droid.Services
+{
+    public class FilePathService : IFilePathService
+    {
+        public string GetLocalFilePath(string fileName)
+        {
+            string path = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+            return System.IO.Path.Combine(path, fileName);
+        }
+    }
+}
