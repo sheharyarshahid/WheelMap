d6fd68ce7ed221ecbfcff184f936df8e7606d78b
Sheharyar Shahid <sshary019@gmail.com>
2020-05-17 03:35:24 +0500

    Added IFilePathService interface

---
+++ WheelMap/WheelMap/Services/IFilePathService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WheelMap.Services
+{
+    public interface IFilePathService
+    {
+        string GetLocalFilePath(string fileName);
+    }
+}
